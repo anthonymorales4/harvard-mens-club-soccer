17ce739b04601580b4010793f6a5db112d16122d
Anthony Morales <anthonymorales@college.harvard.edu>
2025-07-11 17:41:33 -0400

    Commit

--- app/components/ui/alumni/AlumniGrid.js
+++ app/components/ui/alumni/AlumniGrid.js
@@ -7,7 +7,6 @@ export default function AlumniGrid({ alumni, onAlumniClick }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {alumni.map((person, index) => {
-        console.log("person", person);
         const isProfileCreated = person.isProfileCreated;
 
         if (!isProfileCreated) {
